073fac7fff4da3893dc8e0151202eae1e5845c6b
FredyPinedaRodriguez <fgp8000@gmail.com>
2024-12-08 17:11:00 -0500

    EjerciciosClase6

---
+++ src/Ejercicio1.java
@@ -0,0 +1,33 @@
+/*
+ * Ejercicio 1 - Análisis de Ventas de una Tienda
+Eres un analista de datos en una tienda de comercio electrónico. 
+La tienda tiene un registro de todas las ventas realizadas durante 
+el último mes. Cada venta está representada por una cadena de texto
+ que contiene el nombre del producto, la cantidad vendida y el precio
+  unitario, separados por comas. Tu tarea es procesar estos datos para 
+  obtener información útil.
+
+Los datos de entrada son una lista de cadenas de texto, donde cada cadena
+ representa una venta en el formato: "producto,cantidad,precio_unitario".
+  Por ejemplo:
+
+List<String> ventas = Arrays.asList(
+    "camisa,2,20.00",
+    "pantalon,1,35.50",
+    "zapatos,3,50.00",
+    "camisa,1,20.00",
+    "pantalon,2,35.50"
+);
+Tareas
+1. Calcular el Ingreso Total: Calcula el ingreso total generado por todas las ventas.
+2. Contar Ventas por Producto: Cuenta cuántas veces se vendió cada producto.
+3. Filtrar Ventas Mayores a un Monto Específico: Filtra y muestra las ventas 
+  cuyo ingreso total (cantidad * precio_unitario) sea mayor a un monto específico.
+4. Obtener el Producto Más Vendido: Determina cuál fue el producto más vendido en 
+términos de cantidad total.
+ * 
+ */
+
+public class Ejercicio1 {
+    
+}
---
+++ src/Ejercicio2.java
@@ -0,0 +1,35 @@
+/*
+ * Ejercicio 2 - Análisis de Calificaciones de Estudiantes
+Eres un analista de datos en una escuela y tienes un registro de las 
+calificaciones de los estudiantes en diferentes materias. Cada registro
+ está representado por una cadena de texto que contiene el nombre del
+  estudiante, la materia y la calificación, separados por comas. Tu tarea 
+  es procesar estos datos para obtener información útil.
+
+Los datos de entrada son una lista de cadenas de texto, donde cada cadena 
+representa una calificación en el formato: "estudiante,materia,calificacion".
+ Por ejemplo:
+
+List<String> calificaciones = Arrays.asList(
+    "Juan,Matematicas,85",
+    "Ana,Historia,90",
+    "Pedro,Matematicas,70",
+    "Juan,Historia,75",
+    "Ana,Matematicas,95",
+    "Pedro,Historia,80"
+);
+Tareas
+1. Calcular el Promedio de Calificaciones por Estudiante: Calcula el promedio
+de calificaciones para cada estudiante.
+2. Contar Estudiantes por Materia: Cuenta cuántos estudiantes tienen calificaciones
+ en cada materia.
+3. Filtrar Calificaciones Mayores a un Valor Específico: Filtra y muestra las
+ calificaciones que son mayores a un valor específico.
+4. Obtener la Materia con el Promedio Más Alto: Determina cuál es la materia 
+con el promedio de calificaciones más alto.
+ */
+
+
+public class Ejercicio2 {
+    
+}
